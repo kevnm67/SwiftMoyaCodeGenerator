3d8645667c995e6f7746d3b6367738e9992cb18d
kevnm67 <kevnm67@gmail.com>
2019-10-28 10:42:10 -0400

    Request case name formatting
    [Fixed]
    - Naming request cases

--- SwiftMoyaCodeGenerator.js
+++ SwiftMoyaCodeGenerator.js
@@ -1,136 +1,161 @@
 (function() {
-    var uri = require('/URI');
-    var mustache = require('./mustache');
+	var uri = require('/URI');
+	var mustache = require('./mustache');
 
-    var SwiftMoyaCodeGenerator = function() {
+	var SwiftMoyaCodeGenerator = function() {
 
-        this.generate = function(context) {
+		this.generate = function(context) {
 
-            var template = readFile("moya.mustache");
-            var request = context.getCurrentRequest();
-            var apiName = "";
-            var url = uri.parse(request.url);
-            var requestMethod = request.method.toLowerCase();
-            var headers = "";
+			var template = readFile("moya.mustache");
+			var request = context.getCurrentRequest();
+			var apiName = "";
+			var url = uri.parse(request.url);
+			var requestMethod = request.method.toLowerCase();
+			var headers = "";
 
-            var requestParameter = "";
-            var requestName = request.name;
-            var queryParamsType = "";
-            var pathExtension = url.path;
+			var requestParameter = "";
 
-            if (request.parent) {
-                apiName = request.parent.name;
-            }
+			// These define the case names.
+			var requestName = toCamelCase(request.name);
+			var queryParamsType = "";
+			var pathExtension = url.path;
 
-            if (request.name.indexOf('{') > 0) {
-                requestParameter = request.name.match("{([^']+)}")[1];
-                requestName = requestName.replace(request.name.match("{([^']+)}")[0], "");
-                queryParamsType = requestParameter + ": Int";
-            }
+			if (request.parent) {
+				// firstPass = stringifyURL(request.parent.name)
+				// parent_processed = firstPass.replace(request.parent.name.match("{([^']+)}")[0], " ");
+				// apiName = toCamelCase(parent_processed);
+				apiName = toCamelCase(stringifyURL(request.parent.name));
+			}
 
-            requestName = requestName.replace("/", "");
+			if (request.name.indexOf('{') > 0) {
+				requestParameter = request.name.match("{([^']+)}")[1];
+				requestName = requestName.replace(request.name.match("{([^']+)}")[0], "");
+				queryParamsType = requestParameter + ": Int";
+			}
 
-            var pathFragments = pathExtension.split('/');
-            for (var i = 0; i < pathFragments.length; i++) {
-                if (pathFragments[i] == requestName) {
-                    pathExtension = pathExtension.replace(pathFragments[i + 1], "\\(" + requestParameter + ")");
-                    break;
-                }
-            }
+			requestName = requestName.replace("/", "");
 
-            for (header_name in request.headers) {
-                var header_value = request.headers[header_name];
-                if (header_name.toLowerCase() !== 'authorization') {
-                    if (headers) {
-                        headers = headers + ", \n                ";
-                    }
-                    headers = headers + '\"' + header_name + '\" : \"' + header_value + '\"';
-                }
-            }
+			var pathFragments = pathExtension.split('/');
+			for (var i = 0; i < pathFragments.length; i++) {
+				if (pathFragments[i] == requestName) {
+					pathExtension = pathExtension.replace(pathFragments[i + 1], "\\(" + requestParameter + ")");
+					break;
+				}
+			}
+
+			for (header_name in request.headers) {
+				var header_value = request.headers[header_name];
+				if (header_name.toLowerCase() !== 'authorization') {
+					if (headers) {
+						headers = headers + ", \n                ";
+					}
+					headers = headers + '\"' + header_name + '\" : \"' + header_value + '\"';
+				}
+			}
+
+			var view = {
+				"request": request,
+				"baseURL": url.protocol + "://" + url.hostname,
+				"pathExtension": pathExtension,
+				"requestName": requestName,
+				"requestParameter": "let " + requestParameter,
+				"apiName": camelize(apiName),
+				"requestMethod": requestMethod,
+				"headers": headers,
+			};
+
+			var query = url.query;
+
+			if (query) {
+				var fragments = query.split('&');
+				var keyvalue = fragments[0].split('=');
+
+				if (queryParamsType) {
+					queryParamsType = queryParamsType + ", " + keyvalue[0] + ": " + typeForObject(keyvalue[1]);
+				} else {
+					queryParamsType = keyvalue[0] + ": " + typeForObject(keyvalue[1]);
+				}
+
+				var queryParamsTemplate = "_";
+				var queryParams = "let " + keyvalue[0];
+				var queryDictString = "\"" + keyvalue[0] + "\": " + keyvalue[0];
 
 
-            var view = {
-                "request": request,
-                "baseURL": url.protocol + "://" + url.hostname,
-                "pathExtension": pathExtension,
-                "requestName": requestName,
-                "requestParameter": "let " + requestParameter,
-                "apiName": apiName,
-                "requestMethod": requestMethod,
-                "headers": headers,
-            };
+				for (var i = 1; i < fragments.length; i++) {
+					keyvalue = fragments[i].split('=');
+					queryParamsType += ", " + keyvalue[0] + ": " + typeForObject(keyvalue[1]);
+					queryParamsTemplate += ", _";
+					queryParams += ", let " + keyvalue[0];
+					queryDictString += ", \"" + keyvalue[0] + "\": " + keyvalue[0];
+				}
 
+				view["queryParamsTemplate"] = queryParamsTemplate;
+				view["queryParams"] = queryParams;
+				view["queryDictString"] = queryDictString;
+			}
 
+			view["queryParamsType"] = queryParamsType;
 
-            var query = url.query;
-            if (query) {
-                var fragments = query.split('&');
-                var keyvalue = fragments[0].split('=');
+			var jsonBody = request.jsonBody;
+			if (jsonBody && Object.keys(jsonBody).length > 0) {
+				var firstKey = Object.keys(jsonBody)[0];
+				var jsonBodyParamsType = firstKey + ": " + typeForObject(jsonBody[firstKey]);
+				var jsonBodyParamsTemplate = "_";
+				var jsonBodyParams = "let " + firstKey;
+				var jsonBodyDictString = "\"" + firstKey + "\": " + firstKey;
+				for (var i = 1; i < Object.keys(jsonBody).length; i++) {
+					var key = Object.keys(jsonBody)[i];
+					if (jsonBody.hasOwnProperty(key)) {
+						jsonBodyParamsType += ", " + key + ": " + typeForObject(jsonBody[key]);
+						jsonBodyParamsTemplate += ", _";
+						jsonBodyParams += ", let " + key;
+						jsonBodyDictString += ", \"" + key + "\": " + key;
+					}
+				}
 
-                if (queryParamsType) {
-                    queryParamsType = queryParamsType + ", " + keyvalue[0] + ": " + typeForObject(keyvalue[1]);
-                }else{
-                    queryParamsType = keyvalue[0] + ": " + typeForObject(keyvalue[1]);
-                }
-                var queryParamsTemplate = "_";
-                var queryParams = "let " + keyvalue[0];
-                var queryDictString = "\"" + keyvalue[0] + "\": " + keyvalue[0];
+				view["jsonBodyParamsType"] = jsonBodyParamsType;
+				view["jsonBodyParamsTemplate"] = jsonBodyParamsTemplate;
+				view["jsonBodyParams"] = jsonBodyParams;
+				view["jsonBodyDictString"] = jsonBodyDictString;
+			}
 
+			return mustache.render(template, view);
+		}
+	}
 
-                for (var i = 1; i < fragments.length; i++) {
-                    keyvalue = fragments[i].split('=');
-                    queryParamsType += ", " + keyvalue[0] + ": " + typeForObject(keyvalue[1]);
-                    queryParamsTemplate += ", _";
-                    queryParams += ", let " + keyvalue[0];
-                    queryDictString += ", \"" + keyvalue[0] + "\": " + keyvalue[0];
-                }
+	function isNumber(obj) {
+		return !isNaN(parseFloat(obj));
+	}
 
-                view["queryParamsTemplate"] = queryParamsTemplate;
-                view["queryParams"] = queryParams;
-                view["queryDictString"] = queryDictString;
-            }
+	function typeForObject(obj) {
+		return (isNumber(obj) ? "Int" : "String");
+	}
 
-            view["queryParamsType"] = queryParamsType;
+	function camelize(text) {
+		text = text.replace(/[-_\s.]+(.)?/g, (match, c) => c ? c.toUpperCase() : '');
+		return text.substr(0, 1).toLowerCase() + text.substr(1);
+	}
 
-            var jsonBody = request.jsonBody;
-            if (jsonBody && Object.keys(jsonBody).length > 0) {
-                var firstKey = Object.keys(jsonBody)[0];
-                var jsonBodyParamsType = firstKey + ": " + typeForObject(jsonBody[firstKey]);
-                var jsonBodyParamsTemplate = "_";
-                var jsonBodyParams = "let " + firstKey;
-                var jsonBodyDictString = "\"" + firstKey + "\": " + firstKey;
-                for (var i = 1; i < Object.keys(jsonBody).length; i++) {
-                    var key = Object.keys(jsonBody)[i];
-                    if (jsonBody.hasOwnProperty(key)) {
-                        jsonBodyParamsType += ", " + key + ": " + typeForObject(jsonBody[key]);
-                        jsonBodyParamsTemplate += ", _";
-                        jsonBodyParams += ", let " + key;
-                        jsonBodyDictString += ", \"" + key + "\": " + key;
-                    }
-                }
+	function toCamelCase(s) {
+		return s
+			.replace(/_/g, " ")
+			.replace(/\s(.)/g, function($1) {
+				return $1.toUpperCase();
+			})
+			.replace(/\s/g, '')
+			.replace(/^(.)/, function($1) {
+				return $1.toLowerCase();
+			});
+	}
 
-                view["jsonBodyParamsType"] = jsonBodyParamsType;
-                view["jsonBodyParamsTemplate"] = jsonBodyParamsTemplate;
-                view["jsonBodyParams"] = jsonBodyParams;
-                view["jsonBodyDictString"] = jsonBodyDictString;
-            }
+	function stringifyURL(str) {
+		return str.replace(/[ / ]/g, ' ');
+	}
 
-            return mustache.render(template, view);
-        }
-    }
+	SwiftMoyaCodeGenerator.identifier = "com.kjm.PawExtensions.SwiftMoyaCodeGenerator";
+	SwiftMoyaCodeGenerator.title = "Swift (Moya) - KJM";
+	SwiftMoyaCodeGenerator.fileExtension = "swift";
+	SwiftMoyaCodeGenerator.languageHighlighter = "swift";
 
-    function isNumber(obj) {
-        return !isNaN(parseFloat(obj));
-    }
-
-    function typeForObject(obj) {
-        return (isNumber(obj) ? "Int" : "String");
-    }
-
-    SwiftMoyaCodeGenerator.identifier = "com.nam.PawExtensions.SwiftMoyaCodeGenerator";
-    SwiftMoyaCodeGenerator.title = "Swift (Moya)";
-    SwiftMoyaCodeGenerator.fileExtension = "swift";
-    SwiftMoyaCodeGenerator.languageHighlighter = "swift";
-
-    registerCodeGenerator(SwiftMoyaCodeGenerator);
+	registerCodeGenerator(SwiftMoyaCodeGenerator);
 }).call(this);
